4e77c15db17169f8387e29fbbc8520f3066a4728
JadeFront <jadelester.ayat@lorma.edu>
2024-06-17 15:51:17 +0800

    UPDATE

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,95 +8,91 @@ import './Contact.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+
+const schema = yup.object().shape({
+  user_name: yup.string().required('Name is required'),
+  user_email: yup.string().required('Email is required').email('Email is invalid'),
+  message: yup.string().required('Message is required'),
+});
+
 const Contact = () => {
-    useEffect(() => {
-        AOS.init({
-            once: true
+  useEffect(() => {
+    AOS.init({
+      once: true
+    });
+  }, []);
+
+  const form = useRef();
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const sendEmail = (data) => {
+    emailjs
+      .sendForm('service_j1mtqyn', 'template_wmjp0rm', form.current, {
+        publicKey: 'WbUrJdz5ci_0Q6CTK',
+      })
+      .then(() => {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Message sent successfully!",
+          showConfirmButton: false,
+          timer: 3000
         });
-    }, []);
+      }, (error) => {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: "Message failed to send!",
+          showConfirmButton: false,
+          timer: 3000
+        });
+      });
+  };
 
-    const schema = yup.object().shape({
-        fullname: yup.string().required("Fullname required"),
-        email: yup.string().email().required("Email required"),
-        message: yup.string().required("Leave a message")
-    });
+  return (
+    <section id='contact'>
+      <div className='contact-container'>
+        <div className='contact-intro'
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="2000">
+          <h4>Email me</h4>
+          <h1>Let's connect.</h1>
+          <p>
+            Please feel free to send me a message. I'll get in touch with you as soon as possible.
+          </p>
+        </div>
+        <form ref={form} onSubmit={handleSubmit(sendEmail)}
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="2000">
+          
+          <div>
+            <label>Name</label>
+            <input type="text" name="user_name" placeholder='Fullname' {...register('user_name')} />
+            {errors.user_name && <p className="error-message">{errors.user_name.message}</p>}
+          </div>
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: yupResolver(schema)
-    });
+          <div>
+            <label>Email</label>
+            <input type="email" name="user_email" placeholder='Enter email' {...register('user_email')} />
+            {errors.user_email && <p className="error-message">{errors.user_email.message}</p>}
+          </div>
 
-    const form = useRef();
+          <div>
+            <label>Message</label>
+            <textarea name="message" placeholder='Leave a message...' {...register('message')} />
+            {errors.message && <p className="error-message">{errors.message.message}</p>}
+          </div>
 
-    const sendEmail = (data, e) => {
-        e.preventDefault();
-
-        emailjs.sendForm('service_j1mtqyn', 'template_wmjp0rm', form.current, {
-            publicKey: 'WbUrJdz5ci_0Q6CTK',
-          })
-            .then(() => {
-                Swal.fire({
-                    position: "center",
-                    icon: "success",
-                    title: "Message sent successfully!",
-                    showConfirmButton: false,
-                    timer: 3000
-                });
-            }, (error) => {
-                Swal.fire({
-                    position: "center",
-                    icon: "error",
-                    title: "Message failed to send!",
-                    showConfirmButton: false,
-                    timer: 3000
-                });
-            });
-        console.log(data);
-    };
-
-    return (
-        <section id='contact'>
-            <div className='contact-container'>
-                <div className='contact-intro'
-                    data-aos="fade-down"
-                    data-aos-easing="linear"
-                    data-aos-duration="2000">
-                    <h4>Email me</h4>
-                    <h1>Let's connect.</h1>
-                    <p>
-                        Please feel free to send me a message. I'll get in touch with you as soon as possible.
-                    </p>
-                </div>
-
-                <form ref={form} onSubmit={handleSubmit(sendEmail)}
-                    data-aos="fade-down"
-                    data-aos-easing="linear"
-                    data-aos-duration="2000">
-                    <div>
-                        <label>Name</label>
-                        <input type="text" name="user_name" placeholder='Fullname'
-                            {...register('fullname')} />
-                        <p>{errors.fullname?.message}</p>
-                    </div>
-
-                    <div>
-                        <label>Email</label>
-                        <input type="email" name="user_email" placeholder='Email'
-                            {...register('email')} />
-                        <p>{errors.email?.message}</p>
-                    </div>
-
-                    <div>
-                        <label>Message</label>
-                        <textarea name="message" placeholder='Leave a message...'
-                            {...register('message')} />
-                        <p>{errors.message?.message}</p>
-                    </div>
-
-                    <input className='btn' type="submit" value="Send" />
-                </form>
-            </div>
-        </section>
-    );
+          <input className='btn' type="submit" value="Send" />
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
